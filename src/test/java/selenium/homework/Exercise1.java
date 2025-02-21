cadda274806416c1066cbe1ecef1ee01a7ad9e98
adrianag. <adrianag.@mac>
2025-02-21 00:36:51 -0500

    update

---
+++ src/test/java/selenium/homework/Exercise1.java
@@ -0,0 +1,53 @@
+package selenium.homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.Hooks;
+
+public class Exercise1 extends Hooks{
+		
+		public static void main(String[] args) {
+			
+			
+			setUp("safari");
+			
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+			
+			WebElement userName = driver.findElement(By.name("username"));
+			userName.sendKeys("admin");
+			
+			WebElement password = driver.findElement(By.name("password"));
+			password.sendKeys("admin123");
+			
+			WebElement logIn = driver.findElement(By.xpath("//button[@type='submit']"));
+			logIn.click();
+			
+			WebElement headerText = driver.findElement(By.cssSelector("div[class='orangehrm-login-branding']"));
+			
+			String text = headerText.getText();
+			
+			
+			
+			
+			tearDown();
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	
+
+	
+
+		
+
+	
+
+	
+
+}
